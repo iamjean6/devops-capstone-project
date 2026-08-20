169fc3b73b973f6d7fd3ea2e35b0cd3123009854
iamjean6 <powelljean16@gmail.com>
2026-08-20 09:29:32 -0400

    Added CORS headers

--- service/routes.py
+++ service/routes.py
@@ -16,7 +16,7 @@ from . import app  # Import Flask application
 
 @app.route("/health")
 def health():
-    """Health Status"""
+    """Health Status""""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
 
 
