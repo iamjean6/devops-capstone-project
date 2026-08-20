225f5098625800423a623aafa13cecd75b3df492
iamjean6 <powelljean16@gmail.com>
2026-08-20 08:46:29 -0400

    Fix linting errors

--- service/routes.py
+++ service/routes.py
@@ -134,6 +134,7 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
