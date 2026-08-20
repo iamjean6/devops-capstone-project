2fc3c532637ea2ace0921b9c5720e80e395827dd
iamjean6 <powelljean16@gmail.com>
2026-08-20 05:10:21 -0400

    implement list accounts

--- service/routes.py
+++ service/routes.py
@@ -62,21 +62,13 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-    """
-    List all Accounts
-    This endpoint will list all Accounts
-    """
-
-    app.logger.info("Request to list Accounts")
+    """List all Accounts"""
+    app.logger.info("Request to list all Accounts")
 
     accounts = Account.all()
+    results = [account.serialize() for account in accounts]
 
-    account_list = [account.serialize() for account in accounts]
-
-    app.logger.info("Returning [%s] accounts", len(account_list))
-
-    return jsonify(account_list), status.HTTP_200_OK
-
+    return jsonify(results), status.HTTP_200_OK
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
